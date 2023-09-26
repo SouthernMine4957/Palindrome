0d3c230b6550b533d8f17b6d14d15ebdc4c51fa7
SouthernMine4957 <112040234+SouthernMine4957@users.noreply.github.com>
2023-09-26 19:41:14 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string input)
+    {
+        // Remove spaces and convert to lowercase
+        input = input.Replace(" ", "").ToLower();
+
+        // Compare the string with its reverse
+        for (int i = 0, j = input.Length - 1; i < j; i++, j--)
+        {
+            if (input[i] != input[j])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a string to check if it's a palindrome:");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine($"{input} is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"{input} is not a palindrome.");
+        }
+    }
+}
